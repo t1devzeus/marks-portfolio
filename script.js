a95f395d7cb3b3ceb3564b2f968b80df67cc9f24
t1devzeus <coolcaspian76@gmail.com>
2024-08-28 22:38:42 +0200

    origin

--- script.js
+++ script.js
@@ -4,5 +4,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     hamburger.addEventListener('click', function () {
         navBar.classList.toggle('active');
+});
+
+document.querySelector('.about-link').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.querySelector('.experience-link').addEventListener('click', function (e) {
+    e.preventDefault();
+        document.querySelector('#experience').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('.nav-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const target = this.getAttribute('data-target');
+        document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
     });
 });
