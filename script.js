356f3bde843639ad956370751853c46cc484077f
t1devzeus <coolcaspian76@gmail.com>
2024-08-29 19:29:51 +0200

    Updating files

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navBar = document.querySelector('.nav_bar');
-
-    hamburger.addEventListener('click', function () {
-        navBar.classList.toggle('active');
-});
-
 document.querySelector('.about-link').addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector('#about').scrollIntoView({
@@ -17,7 +9,6 @@ document.querySelector('.experience-link').addEventListener('click', function (e
     e.preventDefault();
         document.querySelector('#experience').scrollIntoView({
             behavior: 'smooth'
-        });
     });
 });
 
